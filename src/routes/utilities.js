c6f4c2cbf3990b0427f113624ed4258b57ce4e40
svaniksharma <svaniksharma2@gmail.com>
2026-05-13 20:38:57 -0500

    Formatted js

--- src/routes/utilities.js
+++ src/routes/utilities.js
@@ -1,15 +1,19 @@
-import matter from 'gray-matter'
+import matter from "gray-matter";
 
 export function getPostMetadata() {
-  const importedPosts = import.meta.glob("./posts/*.md", { eager: true, query: "?raw", import: "default" });
+  const importedPosts = import.meta.glob("./posts/*.md", {
+    eager: true,
+    query: "?raw",
+    import: "default",
+  });
   const parsedPosts = Object.entries(importedPosts).map(([path, content]) => {
     const { data, content: markdown } = matter(content);
     return { path, frontmatter: data, markdown };
-  })
+  });
 
   const postMeta = [];
   for (const i in parsedPosts) {
-    const postData = parsedPosts[i]
+    const postData = parsedPosts[i];
     if (postData) {
       postMeta.push({ ...postData.frontmatter, markdown: postData.markdown });
     }
