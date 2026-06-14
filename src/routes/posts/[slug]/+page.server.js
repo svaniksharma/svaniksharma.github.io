ec8f723a26ed21c754c7133c91b68e77730eff3c
svaniksharma <svaniksharma2@gmail.com>
2026-06-13 23:35:28 -0500

    Added mathjax and remarkDirective for rendering

--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -1,35 +1,36 @@
 import { getPostMetadata, parseDate } from "../../utilities";
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import remarkMath from 'remark-math';
-import remarkRehype from 'remark-rehype';
-import rehypeKatex from 'rehype-katex';
-import rehypeHighlight from 'rehype-highlight';
-import rehypeStringify from 'rehype-stringify';
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkMath from "remark-math";
+import remarkRehype from "remark-rehype";
+// import rehypeKatex from 'rehype-katex';
+import remarkDirective from "remark-directive";
+import rehypeMathjax from "rehype-mathjax";
+import rehypeHighlight from "rehype-highlight";
+import rehypeStringify from "rehype-stringify";
 
 export const prerender = true;
 
 const postMeta = getPostMetadata();
 
 async function processMarkdown(markdown) {
-  const result = await unified()
-    .use(remarkParse)
-    .use(remarkMath)
-    .use(remarkRehype)
-    .use(rehypeKatex)
-    .use(rehypeHighlight)
-    .use(rehypeStringify)
-    .process(markdown);
-  return String(result);
+    const result = await unified()
+        .use(remarkParse)
+        .use(remarkDirective)
+        .use(remarkMath)
+        .use(remarkRehype)
+        .use(rehypeMathjax, { tex: { tags: "ams" } })
+        .use(rehypeHighlight)
+        .use(rehypeStringify)
+        .process(markdown);
+    return String(result);
 }
 
 export async function load({ params }) {
-  const post = postMeta.find((post) =>
-    post.slug == params.slug
-  );
-  return {
-    date: parseDate(post.date),
-    title: post.title,
-    markdown: await processMarkdown(post.markdown)
-  };
+    const post = postMeta.find((post) => post.slug == params.slug);
+    return {
+        date: parseDate(post.date),
+        title: post.title,
+        markdown: await processMarkdown(post.markdown),
+    };
 }
